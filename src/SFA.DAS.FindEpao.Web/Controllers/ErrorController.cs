691d708a8aef0b6db65a5e71991cdad62f541f02
Scott Wakefield <scott@expertcc.co.uk>
2020-09-17 09:22:47 +0100

    FAT2-193 use convention for route name.

--- src/SFA.DAS.FindEpao.Web/Controllers/ErrorController.cs
+++ src/SFA.DAS.FindEpao.Web/Controllers/ErrorController.cs
@@ -3,7 +3,7 @@ using SFA.DAS.FindEpao.Web.Infrastructure;
 
 namespace SFA.DAS.FindEpao.Web.Controllers
 {
-    [Route("error")]
+    [Route("[controller]")]
     public class ErrorController : Controller
     {
 
