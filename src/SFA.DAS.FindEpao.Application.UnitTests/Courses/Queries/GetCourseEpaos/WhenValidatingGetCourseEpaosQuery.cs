e95d49770a90cc834641034c250b7a0fa4a961af
Scott Wakefield <scott@expertcc.co.uk>
2020-10-05 10:36:09 +0100

    FAT2-198 validating selected course id.

--- src/SFA.DAS.FindEpao.Application.UnitTests/Courses/Queries/GetCourseEpaos/WhenValidatingGetCourseEpaosQuery.cs
+++ src/SFA.DAS.FindEpao.Application.UnitTests/Courses/Queries/GetCourseEpaos/WhenValidatingGetCourseEpaosQuery.cs
@@ -1,7 +1,57 @@
-﻿namespace SFA.DAS.FindEpao.Application.UnitTests.Courses.Queries.GetCourseEpaos
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.FindEpao.Application.Courses.Queries.GetCourseEpaos;
+using SFA.DAS.FindEpao.Domain.Courses;
+using SFA.DAS.FindEpao.Domain.Interfaces;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.FindEpao.Application.UnitTests.Courses.Queries.GetCourseEpaos
 {
     public class WhenValidatingGetCourseEpaosQuery
     {
-        
+        [Test, MoqAutoData]
+        public async Task And_Course_Not_Found_Then_Invalid(
+            GetCourseEpaosQuery query,
+            CourseList courseList,
+            [Frozen] Mock<ICourseService> mockCourseService,
+            GetCourseEpaosQueryValidator validator)
+        {
+            mockCourseService
+                .Setup(service => service.GetCourses())
+                .ReturnsAsync(courseList);
+
+            var result = await validator.ValidateAsync(query);
+
+            result.IsValid().Should().BeFalse();
+            result.ValidationDictionary.Count.Should().Be(1);
+            result.ValidationDictionary
+                .Should().ContainKey(nameof(GetCourseEpaosQuery.CourseId))
+                .WhichValue.Should().Be($"{nameof(GetCourseEpaosQuery.CourseId)} not found");
+        }
+
+        [Test, MoqAutoData]
+        public async Task And_Course_Is_Found_Then_Valid(
+            GetCourseEpaosQuery query,
+            List<CourseListItem> courses,
+            [Frozen] Mock<ICourseService> mockCourseService,
+            GetCourseEpaosQueryValidator validator)
+        {
+            courses.Add(new CourseListItem(query.CourseId, "title", 2));
+            var courseList = new CourseList
+            {
+                Courses = courses
+            };
+            mockCourseService
+                .Setup(service => service.GetCourses())
+                .ReturnsAsync(courseList);
+
+            var result = await validator.ValidateAsync(query);
+
+            result.IsValid().Should().BeTrue();
+        }
     }
 }
---
+++ src/SFA.DAS.FindEpao.Application/Courses/Queries/GetCourseEpaos/GetCourseEpaosQueryValidator.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using SFA.DAS.FindEpao.Domain.Interfaces;
+using SFA.DAS.FindEpao.Domain.Validation;
+
+namespace SFA.DAS.FindEpao.Application.Courses.Queries.GetCourseEpaos
+{
+    public class GetCourseEpaosQueryValidator : IValidator<GetCourseEpaosQuery>
+    {
+        private readonly ICourseService _courseService;
+
+        public GetCourseEpaosQueryValidator(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        public async Task<ValidationResult> ValidateAsync(GetCourseEpaosQuery item)
+        {
+            var validationResult = new ValidationResult();
+
+            var courseList = await _courseService.GetCourses();
+
+            if (courseList.Courses.All(listItem => listItem.Id != item.CourseId))
+            {
+                validationResult.AddError(
+                    nameof(GetCourseEpaosQuery.CourseId), 
+                    $"{nameof(GetCourseEpaosQuery.CourseId)} not found");
+            }
+
+            return validationResult;
+        }
+    }
+}
