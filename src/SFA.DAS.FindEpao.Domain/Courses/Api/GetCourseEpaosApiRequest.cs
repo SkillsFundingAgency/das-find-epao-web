8e2533bc860e88c5870c51a0f2a7052fb88e4230
Scott Wakefield <scott@expertcc.co.uk>
2020-10-08 15:39:28 +0100

    FAT2-207 adding api request.

---
+++ src/SFA.DAS.FindEpao.Domain/Courses/Api/GetCourseEpaosApiRequest.cs
@@ -0,0 +1,9 @@
+﻿using SFA.DAS.FindEpao.Domain.Interfaces;
+
+namespace SFA.DAS.FindEpao.Domain.Courses.Api
+{
+    public class GetCourseEpaosApiRequest : IGetApiRequest
+    {
+        public string GetUrl { get; }
+    }
+}
