f525ea3cf2e53bc664cae7517e368e2508f22015
Scott Wakefield <scott@expertcc.co.uk>
2020-10-20 10:41:30 +0100

    FAT2-207 adding types for api call.

---
+++ src/SFA.DAS.FindEpao.Domain.UnitTests/Epaos/Api/GetDeliveryAreasApiRequest/WhenGettingGetUrl.cs
@@ -0,0 +1,15 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace SFA.DAS.FindEpao.Domain.UnitTests.Epaos.Api.GetDeliveryAreasApiRequest
+{
+    public class WhenGettingGetUrl
+    {
+        [Test, AutoData]
+        public void Then_Returns_Correct_Path(Domain.Epaos.Api.GetDeliveryAreasApiRequest request)
+        {
+            request.GetUrl.Should().Be("epaos/delivery-areas");
+        }
+    }
+}
---
+++ src/SFA.DAS.FindEpao.Domain/Epaos/DeliveryAreaList.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace SFA.DAS.FindEpao.Domain.Epaos
+{
+    public class DeliveryAreaList
+    {
+        public IEnumerable<DeliveryArea> DeliveryAreas { get; set; }
+    }
+}
