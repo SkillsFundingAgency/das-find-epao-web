adc500f872282379b05147735b8ad723169984c9
Scott Wakefield <scott@expertcc.co.uk>
2020-10-20 12:45:33 +0100

    FAT2-207 adding mock response for delivery areas.

--- src/SFA.DAS.FindEpao.MockServer/MockApiServer.cs
+++ src/SFA.DAS.FindEpao.MockServer/MockApiServer.cs
@@ -36,6 +36,14 @@ namespace SFA.DAS.FindEpao.MockServer
                     .WithHeader("Content-Type", "application/json")
                     .WithBodyFromFile("course-epaos.json"));
 
+            server.Given(Request.Create().WithPath(s => Regex.IsMatch(s,"/epaos/delivery-areas$"))
+                .UsingGet()
+            ).RespondWith(
+                Response.Create()
+                    .WithStatusCode(200)
+                    .WithHeader("Content-Type", "application/json")
+                    .WithBodyFromFile("delivery-areas.json"));
+
             return server;
         }
     }
