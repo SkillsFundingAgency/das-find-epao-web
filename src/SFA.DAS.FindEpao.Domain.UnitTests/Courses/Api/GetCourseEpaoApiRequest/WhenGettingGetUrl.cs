c3db168212581279108d16720aa3c873c2068c1a
Scott Wakefield <scott@expertcc.co.uk>
2020-12-08 15:26:37 +0000

    FAT2-212 added api request.

---
+++ src/SFA.DAS.FindEpao.Domain.UnitTests/Courses/Api/GetCourseEpaoApiRequest/WhenGettingGetUrl.cs
@@ -0,0 +1,15 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace SFA.DAS.FindEpao.Domain.UnitTests.Courses.Api.GetCourseEpaoApiRequest
+{
+    public class WhenGettingGetUrl
+    {
+        [Test, AutoData]
+        public void Then_Returns_Correct_Path(Domain.Courses.Api.GetCourseEpaoApiRequest request)
+        {
+            request.GetUrl.Should().Be($"courses/{request.CourseId}/epaos/{request.EpaoId}");
+        }
+    }
+}
