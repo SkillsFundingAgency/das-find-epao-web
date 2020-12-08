95c871bc93ef0562d855fda361e90b3352794b47
Scott Wakefield <scott@expertcc.co.uk>
2020-12-08 15:18:06 +0000

    FAT2-212 add basic view and view model.

---
+++ src/SFA.DAS.FindEpao.Web/Models/CourseEpaoViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SFA.DAS.FindEpao.Web.Models
+{
+    public class CourseEpaoViewModel
+    {
+        public EpaoListItemViewModel Epao { get; set; } //todo: review type
+        public CourseListItemViewModel Course { get; set; }
+    }
+}
