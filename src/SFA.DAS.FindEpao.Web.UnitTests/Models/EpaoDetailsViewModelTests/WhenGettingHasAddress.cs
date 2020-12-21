3890bd683890076e590f9faaa26e6b1a767b5851
Scott Wakefield <scott@expertcc.co.uk>
2020-12-21 17:51:06 +0000

    FAT2-212 added markup, couple of view model tweaks.

---
+++ src/SFA.DAS.FindEpao.Web.UnitTests/Models/EpaoDetailsViewModelTests/WhenGettingHasAddress.cs
@@ -0,0 +1,63 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.FindEpao.Web.Models;
+
+namespace SFA.DAS.FindEpao.Web.UnitTests.Models.EpaoDetailsViewModelTests
+{
+    public class WhenGettingHasAddress
+    {
+        [Test]
+        public void And_Address_Fields_Then_False()
+        {
+            var contactDetails = new EpaoContactDetailsViewModel();
+
+            contactDetails.HasAddress().Should().BeFalse();
+        }
+
+        [Test, AutoData]
+        public void And_Has_Address1_Then_True(
+            string address1)
+        {
+            var contactDetails = new EpaoContactDetailsViewModel{Address1 = address1};
+
+            contactDetails.HasAddress().Should().BeTrue();
+        }
+
+        [Test, AutoData]
+        public void And_Has_Address2_Then_True(
+            string address2)
+        {
+            var contactDetails = new EpaoContactDetailsViewModel{Address2 = address2};
+
+            contactDetails.HasAddress().Should().BeTrue();
+        }
+
+        [Test, AutoData]
+        public void And_Has_Address3_Then_True(
+            string address3)
+        {
+            var contactDetails = new EpaoContactDetailsViewModel{Address3 = address3};
+
+            contactDetails.HasAddress().Should().BeTrue();
+        }
+
+        [Test, AutoData]
+        public void And_Has_Address4_Then_True(
+            string address4)
+        {
+            var contactDetails = new EpaoContactDetailsViewModel{Address4 = address4};
+
+            contactDetails.HasAddress().Should().BeTrue();
+        }
+
+        [Test, AutoData]
+        public void And_Has_Postcode_Then_True(
+            string postcode)
+        {
+            var contactDetails = new EpaoContactDetailsViewModel{Postcode = postcode};
+
+            contactDetails.HasAddress().Should().BeTrue();
+        }
+    }
+}
--- src/SFA.DAS.FindEpao.Web/Models/CourseEpaoViewModel.cs
+++ src/SFA.DAS.FindEpao.Web/Models/CourseEpaoViewModel.cs
@@ -1,66 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using SFA.DAS.FindEpao.Domain.Courses;
-using SFA.DAS.FindEpao.Domain.Epaos;
-
-namespace SFA.DAS.FindEpao.Web.Models
+﻿namespace SFA.DAS.FindEpao.Web.Models
 {
     public class CourseEpaoViewModel
     {
         public CourseListItemViewModel Course { set; get; }
         public EpaoDetailsViewModel Epao { get; set; }
         public int CourseEpaosCount { get; set; }
-    }
-
-    public class EpaoDetailsViewModel : EpaoWithLocation
-    {
-        public EpaoDetailsViewModel(
-            EpaoDetails epao,
-            IReadOnlyList<EpaoDeliveryArea> epaoDeliveryAreas, 
-            IReadOnlyList<DeliveryArea> deliveryAreas, 
-            Func<IReadOnlyList<EpaoDeliveryArea>, IReadOnlyList<DeliveryArea>, string> buildLocations) 
-            : base(epaoDeliveryAreas, deliveryAreas, buildLocations)
-        {
-            EpaoId = epao.EpaoId;
-            Name = epao.Name;
-            PrimaryContactName = epao.PrimaryContactName;
-            ContactDetails = epao.ContactDetails;
-        }
-
-        public string EpaoId { get; }
-        public string Name { get; }
-        public string PrimaryContactName { get; }
-        public EpaoContactDetailsViewModel ContactDetails { get; }
-    }
-
-    public class EpaoContactDetailsViewModel
-    {
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
-        public string WebsiteLink { get; set; }
-
-        public string Address1 { get; set; }
-        public string Address2 { get; set; }
-        public string Address3 { get; set; }
-        public string Address4 { get; set; }
-        public string Postcode { get; set; }
-
-        public static implicit operator EpaoContactDetailsViewModel(
-            EpaoContactDetails source)
-        {
-            if (source is null) return default;
-
-            return new EpaoContactDetailsViewModel
-            {
-                Email = source.Email,
-                PhoneNumber = source.PhoneNumber,
-                WebsiteLink = source.WebsiteLink,
-                Address1 = source.Address1,
-                Address2 = source.Address2,
-                Address3 = source.Address3,
-                Address4 = source.Address4,
-                Postcode = source.Postcode
-            };
-        }
     }
 }
