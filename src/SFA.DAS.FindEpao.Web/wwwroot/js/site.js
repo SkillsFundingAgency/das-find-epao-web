e04f3ed85e11a60e0ac2c66cc0a713edc726659e
Scott Wakefield <scott@expertcc.co.uk>
2020-12-22 16:40:18 +0000

    FAT2-332 adding js.

--- src/SFA.DAS.FindEpao.Web/wwwroot/js/site.js
+++ src/SFA.DAS.FindEpao.Web/wwwroot/js/site.js
@@ -42,3 +42,77 @@ if (selectEl) {
 }
 
 forms.attr('novalidate', 'novalidate');
+
+
+
+// BACK LINK
+// If users history-1 does not come from this site, 
+// then show a link to homepage
+
+var $backLinkOrHome = $('.das-js-back-link-or-home');
+var backLinkOrHome = function () {
+
+    var referrer = document.referrer;
+
+    var backLink = $('<a>')
+        .attr({'href': '#', 'class': 'govuk-back-link'})
+        .text('Back')
+        .on('click', function (e) {
+            window.history.back();
+            e.preventDefault();
+        });
+
+    if (referrer && referrer !== document.location.href) {
+        $backLinkOrHome.replaceWith(backLink);
+    }
+}
+
+if ($backLinkOrHome) {
+    backLinkOrHome();
+}
+
+
+// BACK TO TOP 
+// Shows a back-to-top link in a floating header
+// as soon as the breadcrumbs scroll out of view
+
+$(window).bind('scroll', function() {
+
+    var isCookieBannerVisible = $('.das-cookie-banner:visible').length,
+        showHeaderDistance = 150 + (isCookieBannerVisible * 240),
+        $breadcrumbs = $('.govuk-breadcrumbs');
+
+    if ($breadcrumbs.length > 0) {
+        var breadcrumbDistanceFromTop = $breadcrumbs.offset().top,
+            breadcrumbHeight = $breadcrumbs.outerHeight();
+
+        showHeaderDistance = breadcrumbDistanceFromTop + breadcrumbHeight;
+    }
+
+    if ($(window).scrollTop() > showHeaderDistance) {
+        $('.app-shortlist-banner').addClass('app-shortlist-banner__fixed');
+    } else {
+        $('.app-shortlist-banner').removeClass('app-shortlist-banner__fixed');
+    }
+
+}).trigger("scroll");
+
+
+// SCROLL TO TARGET 
+// On click of the link, checks to see if the target exists
+// If so, scrolls the page to that point, taking into account
+// the back-to-top header
+
+$("a[data-scroll-to-target]").on('click', function () {
+    var target = $(this).data('scroll-to-target'),
+        $target = $(target);
+        headerOffset = $('.app-shortlist-banner__fixed').outerHeight() || 50;
+
+    setTimeout(function() {
+        if ($target.length > 0) {
+            var scrollTo = $target.offset().top - headerOffset;
+            $('html, body').animate({scrollTop: scrollTo}, 0);
+        }
+    }, 10)
+
+});
