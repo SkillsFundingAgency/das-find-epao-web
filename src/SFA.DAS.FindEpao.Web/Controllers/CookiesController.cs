05af15df421d35f423e1f29203d2b8f88faad673
Peter Williams <peter.david.williams.1991@gmail.com>
2021-01-20 11:55:24 +0000

    Added privacy page and moved cookies pages to make back/home links work

---
+++ src/SFA.DAS.FindEpao.Web/Controllers/CookiesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.FindEpao.Web.Infrastructure;
+
+namespace SFA.DAS.FindEpao.Web.Controllers
+{
+    public class CookiesController : Controller
+    {
+        [Route("cookies", Name = RouteNames.Cookies)]
+        public IActionResult Cookies()
+        {
+            return View();
+        }
+
+        [Route("cookies-details", Name = RouteNames.CookiesDetails)]
+        public IActionResult CookiesDetails()
+        {
+            return View();
+        }
+    }
+}
