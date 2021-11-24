eefa249a03902b994a57a4d6d1c9aeb0c410c544
anilkdhiman6 <anil.dhiman@digital.education.gov.uk>
2021-11-24 18:00:44 +0000

    remove unnecessary usings

--- src/SFA.DAS.FindEpao.Web/Extensions/StandardVersionsFormatExtensions.cs
+++ src/SFA.DAS.FindEpao.Web/Extensions/StandardVersionsFormatExtensions.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace SFA.DAS.FindEpao.Web.Extensions
+﻿namespace SFA.DAS.FindEpao.Web.Extensions
 {
     public static class StandardVersionsFormatExtensions
     {
