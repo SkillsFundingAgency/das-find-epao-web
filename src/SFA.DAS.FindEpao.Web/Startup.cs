5ed87468087dccae2657b51db5829937572fedf8
shaun-downey-education <shaun.downey@education.gov.uk>
2024-12-02 10:51:47 +0000

    CSP-1720: Remove unnecessary using.

--- src/SFA.DAS.FindEpao.Web/Startup.cs
+++ src/SFA.DAS.FindEpao.Web/Startup.cs
@@ -4,7 +4,6 @@ using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
